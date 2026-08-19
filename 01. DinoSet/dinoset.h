8425d6df0a499526dcda27aac0d91e7983860942
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-08-19 22:51:36 +0000

    Add initial source code for DinoSet lab

---
+++ 01. DinoSet/dinoset.h
@@ -0,0 +1,29 @@
+#pragma once
+
+const int total_dinos = 8;
+
+enum class DinoId {
+    velociraptor,        // 0
+    stegosaurus,         // 1
+    tyrannosaurus,       // 2
+    procompsognathus,    // 3
+    triceratops,         // 4
+    pachycephalosaurus,  // 5
+    parasaurolophus,     // 6
+    pteranodon           // 7
+};
+
+class DinoSet {
+
+public:
+
+    void add(DinoId id)
+    {
+        int index = static_cast<int>(id);
+        _dino[index] = true;
+    }
+
+private:
+
+    bool _dino[total_dinos] {};
+};
---
+++ 01. DinoSet/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dinoset.h"
+
+int main()
+{
+    DinoSet a;
+    a.add(DinoId::velociraptor);
+    a.add(DinoId::pteranodon);
+    
+    return 0;
+}
