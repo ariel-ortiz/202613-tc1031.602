6c9338228afff70454125582e90646214ce899c0
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-08-21 22:45:24 +0000

    Add code to implement contains, to_string and operator+ for DinoSet class

--- 01. DinoSet/dinoset.h
+++ 01. DinoSet/dinoset.h
@@ -23,6 +23,37 @@ public:
         _dino[index] = true;
     }
 
+    bool contains(DinoId id) const
+    {
+        int index = static_cast<int>(id);
+        return _dino[index];
+    }
+
+    DinoSet operator+(const DinoSet& other) const
+    {
+        DinoSet result;
+        for (int i = 0; i < total_dinos; i++) {
+            DinoId id = static_cast<DinoId>(i);
+            if (contains(id) or other.contains(id)) {
+                result.add(id);
+            }
+        }
+        return result;
+    }
+
+    std::string to_string() const
+    {
+        std::string result = "";
+        for (int i = 0; i < total_dinos; i++) {
+            if (_dino[i]) {
+                result += "1";
+            } else {
+                result += "0";
+            }
+        }
+        return result;
+    }
+
 private:
 
     bool _dino[total_dinos] {};
--- 01. DinoSet/main.cpp
+++ 01. DinoSet/main.cpp
@@ -3,9 +3,20 @@
 
 int main()
 {
-    DinoSet a;
+    DinoSet a, b, c;
+    
     a.add(DinoId::velociraptor);
     a.add(DinoId::pteranodon);
+    a.add(DinoId::tyrannosaurus);
     
+    b.add(DinoId::tyrannosaurus);
+    b.add(DinoId::stegosaurus);
+    
+    c = a + b;
+    
+    std::cout << "a = " << a.to_string() << "\n";
+    std::cout << "b = " << b.to_string() << "\n";
+    std::cout << "c = " << c.to_string() << "\n";
+
     return 0;
 }
