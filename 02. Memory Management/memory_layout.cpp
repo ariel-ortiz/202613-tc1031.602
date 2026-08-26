02703c4885a9b98aefe245fcb96181a5c3a35a9d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-08-26 22:52:24 +0000

    Add initial implementation of memory layout demonstration

---
+++ 02. Memory Management/memory_layout.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int c = 3;
+int d = 4;
+
+void some_fun()
+{
+}
+
+int e;
+
+const int a = 1;
+const int b = 2;
+
+int f;
+
+int main()
+{
+    int *g = new int(5);
+    int *h = new int(6);
+
+    int i;
+    int j;
+
+    std::cout << "Text Segment\n";
+    std::cout << "&some_fun = " << reinterpret_cast<void*>(&some_fun) << "\n";
+    std::cout << "&main     = " << reinterpret_cast<void*>(&main) << "\n\n";
+
+    std::cout << "Read Only Data Segment\n";
+    std::cout << "&a        = " << &a << "\n";
+    std::cout << "&b        = " << &b << "\n\n";
+
+    std::cout << "Initialized Data Segment\n";
+    std::cout << "&c        = " << &c << "\n";
+    std::cout << "&d        = " << &d << "\n\n";
+
+    std::cout << "Uninitialized Data Segment\n";
+    std::cout << "&e        = " << &e << "\n";
+    std::cout << "&f        = " << &f << "\n\n";
+
+    std::cout << "Heap\n";
+    std::cout << "g         = " << g << "\n";
+    std::cout << "h         = " << h << "\n\n";
+
+    delete g;
+    delete h;
+
+    std::cout << "Stack\n";
+    std::cout << "&i        = " << &i << "\n";
+    std::cout << "&j        = " << &j << "\n\n";
+
+    return 0;
+}
