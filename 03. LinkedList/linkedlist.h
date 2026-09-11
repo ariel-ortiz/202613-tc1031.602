18fbd2bc39adcdaff03faf345a4bdb4e8e27494a
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2026-09-11 22:48:08 +0000

    Implement additional LinkedList functionalities: add destructor, insert_back, is_empty, remove_front, and to_string methods; update tests accordingly

--- 03. LinkedList/linkedlist.h
+++ 03. LinkedList/linkedlist.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 
 template<typename T>
 class LinkedList {
@@ -13,6 +15,38 @@ public:
         _sentinel = new Node;
         _sentinel->next = _sentinel;
         _sentinel->prev = _sentinel;
+    }
+
+    // Complexity: O(N)
+    LinkedList(std::initializer_list<T> args): LinkedList()
+    {
+        for (T arg : args) {
+            insert_back(arg);
+        }
+    }
+
+    // Complexity: O(N)
+    ~LinkedList()
+    {
+        Node* p = _sentinel->next;
+        while (p != _sentinel) {
+            Node* q = p;
+            p = p->next;
+            delete q;
+        }
+        delete _sentinel;
+    }
+
+    // Complexity: O(1)
+    void insert_back(T value)
+    {
+        Node* new_node = new Node;
+        new_node->value = value;
+        new_node->prev = _sentinel->prev;
+        new_node->next = _sentinel;
+        _sentinel->prev->next = new_node;
+        _sentinel->prev = new_node;
+        _size++;
     }
 
     // Complexity: O(1)
@@ -33,6 +67,47 @@ public:
         return _size;
     }
 
+    // Complexity: O(1)
+    bool is_empty() const
+    {
+        return _size == 0;
+    }
+
+    // Complexity: O(1)
+    T remove_front()
+    {
+        if (is_empty()) {
+            throw std::length_error("Can't remove front from an empty list");
+        }
+        Node* p = _sentinel->next;
+        T result = p->value;
+        _sentinel->next = p->next;
+        p->next->prev = _sentinel;
+        delete p;
+        _size--;
+        return result;
+    }
+
+    // Complexity: O(N)
+    std::string to_string() const
+    {
+        std::ostringstream result;
+        result << "[";
+        bool first_time = true;
+        Node* p = _sentinel->next;
+        while (p != _sentinel) {
+            if (first_time) {
+                first_time = false;
+            } else {
+                result << ", ";
+            }
+            result << p->value;
+            p = p->next;
+        }
+        result << "]";
+        return result.str();
+    }
+
 private:
 
     struct Node {
@@ -44,3 +119,10 @@ private:
     int _size = 0;
     Node* _sentinel = nullptr;
 };
+
+// Complexity: O(N)
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const LinkedList<T>& list)
+{
+    return os << list.to_string();
+}
--- 03. LinkedList/main.cpp
+++ 03. LinkedList/main.cpp
@@ -3,5 +3,15 @@
 
 int main()
 {
+    LinkedList<int> a {1, 2, 3};
+    a.insert_front(5);
+    a.insert_front(7);
+    a.insert_front(6);
+    a.insert_back(8);
+    a.insert_back(9);
+    a.insert_back(10);
+    std::cout << a << "\n";
+    std::cout << a.remove_front() << "\n";
+    std::cout << a << "\n";
     return 0;
 }
